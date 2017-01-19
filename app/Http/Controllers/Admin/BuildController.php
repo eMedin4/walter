38f87de3b673ecc18814330f79829d7d2742f40e
eMedin4 <elannmedina@gmail.com>
2017-01-19 06:50:27 +0100

    '0.5002'

--- app/Http/Controllers/Admin/BuildController.php
+++ app/Http/Controllers/Admin/BuildController.php
@@ -95,7 +95,7 @@ class BuildController extends Controller
 			} 
 
 			//SCRAPEAMOS
-			$filterScore = 1000;	//MINIMO DE VOTOS PARA SCRAPEAR
+			$filterScore = 1;	//MINIMO DE VOTOS PARA SCRAPEAR
 			$results = array_merge($results, $this->scraper->scrapList($i, $client, $crawler, $order, $filterScore, $configTmdb));
 		}
 
