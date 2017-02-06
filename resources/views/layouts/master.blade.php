2282ac956a5d1a271f38cd957745b52cf336ce5c
eMedin4 <elannmedina@gmail.com>
2017-02-06 22:21:11 +0100

    0.5014

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,6 +19,16 @@
 	<meta property="og:description"     content="@yield('og_description')" />
 	@yield('more_og')
 
+	<!-- Google analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-91524304-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+
 	<script src="https://use.fortawesome.com/712aad58.js"></script>
 	<link rel="stylesheet" href="{{ asset('/assets/css/style2.css') }}">
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet">
